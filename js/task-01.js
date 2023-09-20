1fd75bee63a1ce80ff93209ed0125431d854a62e
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2023-09-20 23:54:39 +0300

    done with task#01

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
-console.log("Number of categories:");
-console.log("Category:");
-console.log("Elements:");
-console.log("Category:");
-console.log("Elements:");
-console.log("Category:");
-console.log("Elements:");
+const categoriesList = document.querySelectorAll(".item");
+console.log("Number of categories:", categoriesList);
+
+categoriesList.forEach((item) => {
+  console.log("Category:", item.firstChild.nextElementSibling.innerText);
+  console.log("Elements:", item.children[1].children.length);
+});
